8201db71ab083668c6bbae18509ca48aa0ececc9
whlsls3377 <whlsls3377@naver.com>
2024-11-01 20:54:12 +0900

    first init

---
+++ src/test/java/com/sparta/realtomatoapp/RealtomatoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.realtomatoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtomatoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
