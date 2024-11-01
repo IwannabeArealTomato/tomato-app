9d6dc0ee856d642414fabd249fdbb8203cdd6e01
whlsls3377 <whlsls3377@naver.com>
2024-11-01 20:52:40 +0900

    first init

---
+++ src/main/java/com/sparta/realtomatoapp/RealtomatoappApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.realtomatoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtomatoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtomatoappApplication.class, args);
+    }
+
+}
