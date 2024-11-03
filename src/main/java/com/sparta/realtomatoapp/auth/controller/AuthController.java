e84773debedca42e77dec48a6f2ba6274d28523e
gebeen-dev <rrdd1207@gmail.com>
2024-11-03 16:37:35 +0900

    update : AuthController 로그인 검증

--- src/main/java/com/sparta/realtomatoapp/auth/controller/AuthController.java
+++ src/main/java/com/sparta/realtomatoapp/auth/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.sparta.realtomatoapp.auth.controller;
 
+import com.sparta.realtomatoapp.auth.config.JwtProvider;
+import com.sparta.realtomatoapp.auth.dto.AuthInfo;
 import com.sparta.realtomatoapp.auth.dto.LoginDto;
+import com.sparta.realtomatoapp.domain.user.entity.UserRole;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private final JwtProvider jwtProvider;
+
     @PostMapping("/login")
     public String login(@RequestBody LoginDto loginDto) {
         log.info("AuthController.login");
-        return "SUCCESS";
+        AuthInfo build = AuthInfo.builder()
+                .email(loginDto.getEmail())
+                .role(String.valueOf(UserRole.GUEST))
+                .build();
+
+        String jwtToken = jwtProvider.createJwtToken(build);
+        return jwtToken;
     }
 
 }
