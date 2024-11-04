20b142c5028ee5c5d8a89afff7183fa4704132b4
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-04 17:10:16 +0900

    fix: login 메서드 반환형 변경

--- src/main/java/com/sparta/realtomatoapp/auth/controller/AuthController.java
+++ src/main/java/com/sparta/realtomatoapp/auth/controller/AuthController.java
@@ -2,14 +2,13 @@ package com.sparta.realtomatoapp.auth.controller;
 
 import com.sparta.realtomatoapp.auth.dto.UserRegistrationRequestDto;
 import com.sparta.realtomatoapp.auth.dto.UserResponseDto;
-import com.sparta.realtomatoapp.common.dto.ApiResponseDto;
-import com.sparta.realtomatoapp.security.config.JwtProvider;
-import com.sparta.realtomatoapp.auth.dto.AuthInfo;
+import com.sparta.realtomatoapp.common.dto.BaseResponseDto;
+import com.sparta.realtomatoapp.common.dto.DataResponseDto;
 import com.sparta.realtomatoapp.auth.dto.LoginDto;
-import com.sparta.realtomatoapp.user.entity.UserRole;
 import com.sparta.realtomatoapp.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,31 +20,46 @@ import java.util.Collections;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final JwtProvider jwtProvider;
     private final UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<BaseResponseDto> login(@RequestBody LoginDto request) {
         log.info("AuthController.login");
 
         try {
-            String jwtToken = userService.loginUser(loginDto.getEmail(), loginDto.getPassword());
-            return ResponseEntity.ok(jwtToken);
+            String jwtToken = userService.loginUser(request);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Bearer " + jwtToken);
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(
+                            BaseResponseDto.baseResponseBuilder()
+                                .message("로그인 성공")
+                                .build()
+                    );
+
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.badRequest()
+                    .body(
+                            BaseResponseDto.baseResponseBuilder()
+                            .message("로그인 실패" + e.getMessage())
+                            .build()
+                    );
         }
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponseDto> signup(@RequestBody UserRegistrationRequestDto request) {
+    public ResponseEntity<DataResponseDto> signup(@RequestBody UserRegistrationRequestDto request) {
         log.info("AuthController.signup");
 
         // 사용자 등록
-        UserResponseDto userResponseDTO = userService.registerUser(request);
+        UserResponseDto userResponseDto = userService.registerUser(request);
 
-        return ResponseEntity.ok(ApiResponseDto.<UserResponseDto>builder()
+        return ResponseEntity.ok(DataResponseDto.<UserResponseDto>dataResponseBuilder()
                 .message("회원 가입 성공")
-                .data(Collections.singletonList(userResponseDTO))
+                .data(Collections.singletonList(userResponseDto))
                 .build());
     }
 }
