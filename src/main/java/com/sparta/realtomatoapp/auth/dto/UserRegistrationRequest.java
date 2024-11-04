9b23e16e1c2ccea2a43148a9e8a1da5b9e24d7bc
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-04 14:43:26 +0900

    update: 회원 가입 시 userRole 속성 추가

--- src/main/java/com/sparta/realtomatoapp/auth/dto/UserRegistrationRequest.java
+++ src/main/java/com/sparta/realtomatoapp/auth/dto/UserRegistrationRequest.java
@@ -1,13 +1,14 @@
 package com.sparta.realtomatoapp.auth.dto;
 
+import com.sparta.realtomatoapp.user.entity.UserRole;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 public class UserRegistrationRequest {
     private String userName;
     private String email;
     private String password;
+    private String userRole;
     private String address;
 }
