53e79a0325f49bf9d96f651a183aaacbea3d53ed
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-04 14:42:47 +0900

    update: Getter 어노테이션 추가

--- src/main/java/com/sparta/realtomatoapp/common/entity/BaseAuditingEntity.java
+++ src/main/java/com/sparta/realtomatoapp/common/entity/BaseAuditingEntity.java
@@ -1,11 +1,13 @@
 package com.sparta.realtomatoapp.common.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter // 하위 엔티티에서 생성일을 가져오기 위함.
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseAuditingEntity {
