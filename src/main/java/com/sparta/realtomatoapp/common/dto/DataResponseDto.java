d0d9443edb7df920f54a015cab0c000dfb652f4b
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-04 17:07:10 +0900

    fix: 공통 Response dto 를 data가 포함된 dto 와 message 만 포함한 dto로 분리

--- src/main/java/com/sparta/realtomatoapp/common/dto/DataResponseDto.java
+++ src/main/java/com/sparta/realtomatoapp/common/dto/DataResponseDto.java
@@ -1,15 +1,16 @@
 package com.sparta.realtomatoapp.common.dto;
 
+import jakarta.persistence.MappedSuperclass;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
-@Builder
-public abstract class DataResponseDto<T> extends BaseApiResponseDto<T> {
+public class DataResponseDto<T> extends BaseResponseDto {
     private List<T> data;
 
+    @Builder(builderMethodName = "dataResponseBuilder")
     public DataResponseDto(String message, List<T> data) {
         super(message);
         this.data = data;
