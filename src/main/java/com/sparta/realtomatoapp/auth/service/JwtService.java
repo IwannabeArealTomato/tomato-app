434dd5c74b22c44402dd478a6819f4e4580a7f34
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-04 11:31:47 +0900

    del: JwtService 삭제

--- src/main/java/com/sparta/realtomatoapp/auth/service/JwtService.java
+++
@@ -1,6 +0,0 @@
-package com.sparta.realtomatoapp.auth.service;
-
-public class JwtService {
-    //RefreshTokenService가 포함되어 Refresh Token Rotation을 구현
-    //보안 위험을 줄이기 위해 refresh token이 사용될 때마다 갱신하도록 설계 할 예정
-}
