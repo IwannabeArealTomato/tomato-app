ca80063cf8520ba66575ddb07d0e6c14d3ca11db
GoUiJeong <uijeong0525@gmail.com>
2024-11-04 14:42:25 +0900

    add : store 단건 조회 API

---
+++ src/main/java/com/sparta/realtomatoapp/store/dto/StoreResponseDTO.java
@@ -0,0 +1,24 @@
+package com.sparta.realtomatoapp.store.dto;
+
+import com.sparta.realtomatoapp.store.entity.Store;
+import com.sparta.realtomatoapp.store.entity.StoreStatus;
+import lombok.Getter;
+
+@Getter
+public class StoreResponseDTO {
+    private Long storeId;
+    private String storeName;
+    private String openTime;
+    private String closeTime;
+    private Long minPrice;
+    private StoreStatus status;
+
+    public StoreResponseDTO(Store store) {
+        this.storeId = store.getStoreId();
+        this.storeName = store.getStoreName();
+        this.openTime = store.getOpenTime().toString();
+        this.closeTime = store.getCloseTime().toString();
+        this.minPrice = store.getMinPrice();
+        this.status = store.getStatus();
+    }
+}
