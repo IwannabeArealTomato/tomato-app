d2021a99c984ae4bf7c4d519144652038b9a4894
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-04 14:55:59 +0900

    refactor: 주석 제거

--- src/main/java/com/sparta/realtomatoapp/common/dto/ApiResponse.java
+++ src/main/java/com/sparta/realtomatoapp/common/dto/ApiResponse.java
@@ -1,6 +1,5 @@
 package com.sparta.realtomatoapp.common.dto;
 
-import com.sparta.realtomatoapp.auth.dto.UserResponseDto;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,5 +9,5 @@ import java.util.List;
 @Builder
 public class ApiResponse<T> {
     private String message;
-    private List<T> data; //todo : 조금 더 범용적으로 바꿔야함. 현재는 유저 DTO 밖에 못가져옴
+    private List<T> data;
 }
