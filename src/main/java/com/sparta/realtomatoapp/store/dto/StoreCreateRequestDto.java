f8df59d0f78bf632dfa228b37c8deac3ff37f384
GoUiJeong <uijeong0525@gmail.com>
2024-11-04 16:52:48 +0900

    fix : dto를 data 출력 양식에 맞게 수정.

--- src/main/java/com/sparta/realtomatoapp/store/dto/StoreCreateRequestDto.java
+++ src/main/java/com/sparta/realtomatoapp/store/dto/StoreCreateRequestDto.java
@@ -4,30 +4,17 @@ import com.sparta.realtomatoapp.store.entity.StoreStatus;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalTime;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoreCreateRequestDto {
-
-    @NotNull(message = "가게 이름은 필수 입니다.")
-    @Size(min = 3, max = 20, message = "가게 이름은 3글자 이상 20글자 이하여야 합니다.")
     private String storeName;
-
-    @NotNull(message = "개장 시간을 필수로 입력하셔야 합니다.")
     private LocalTime openTime;
-
-    @NotNull(message = "마감 시간을 필수로 입력하셔야 합니다.")
     private LocalTime closeTime;
-
-    @NotNull(message = "최소 주문 금액을 필수로 입력하셔야 합니다.")
-    @Min(value = 15000, message = "최소 주문 금액은 15000원 이상이어야 합니다.")
     private Long minPrice;
-
-    //가게 상태
-    @NotNull(message = "가게 상태를 입력해 주세요.")
     private StoreStatus status;
 }
--- src/main/java/com/sparta/realtomatoapp/store/dto/StoreDeleteResponseDto.java
+++ src/main/java/com/sparta/realtomatoapp/store/dto/StoreDeleteResponseDto.java
@@ -1,16 +1,12 @@
 package com.sparta.realtomatoapp.store.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class StoreDeleteResponseDto {
     private String message;
-    private Long deletedStoreId;
-
-    public StoreDeleteResponseDto(Long storeId) {
-        this.message = "가게 삭제 성공";
-        this.deletedStoreId = storeId;
-    }
 }
