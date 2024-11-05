c7992a41c5f12790e336c350a0950b6f284126eb
whlsls3377 <whlsls3377@naver.com>
2024-11-05 15:45:42 +0900

    refactor : 리프레쉬 토큰을 위한 jwt 설정 수정

--- src/main/java/com/sparta/realtomatoapp/security/config/JwtConfig.java
+++ src/main/java/com/sparta/realtomatoapp/security/config/JwtConfig.java
@@ -2,21 +2,19 @@ package com.sparta.realtomatoapp.security.config;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Getter
 @Component
 @RequiredArgsConstructor
 public class JwtConfig {
 
-    // 토큰의 만료 시간 (분 단위)
-    @Value("${acess.token.expire.time}")
-    private Integer jwtaccesstokenexpiretime;
+    // Access Token 만료 시간 (분 단위)
+    @Value("${access.token.expire.time}")
+    private long accessTokenExpireTime;
 
-    // 서명 키 (secret key)
-    @Value("${acess.token.secret.key}")
-    private String jwtaccessTokenSecretKey;
+    // Refresh Token 만료 시간 (분 단위)
+    @Value("${refresh.token.expire.time}")
+    private long refreshTokenExpireTime;
 }
--- src/main/java/com/sparta/realtomatoapp/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/realtomatoapp/security/filter/JwtAuthenticationFilter.java
@@ -16,42 +16,45 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    //AdminTokenInterceptor는 관리자 권한을 가진 사용자가 접근할 수 있는 특정 경로에서 추가적인 인증 절차를 통해 보안을 강화함
 
     private final JwtProvider jwtProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //1. 토큰이 필요없는 API
         String requestURI = request.getRequestURI();
         if (requestURI.contains("/api/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        //2. 토큰 검증
         String bearerToken = request.getHeader("Authorization");
         String token = null;
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            token =bearerToken.substring("Bearer ".length()); // "Bearer "의 길이가 7이므로 그 뒤의 문자열을 반환
+            token = bearerToken.substring("Bearer ".length());
         }
 
-        if (!jwtProvider.verifyAccessToken(token)) {
-            // 사용자에 잘못된 값 json으로 날림
-            unAuthResponse(response);
+        if (token != null && jwtProvider.verifyAccessToken(token)) {
+            filterChain.doFilter(request, response);
             return;
         }
 
-        filterChain.doFilter(request, response);
+        String refreshToken = request.getHeader("Refresh-Token");
+        if (refreshToken != null && jwtProvider.verifyRefreshToken(refreshToken)) {
+            String username = jwtProvider.getUserFromRefreshToken(refreshToken);
+            String newAccessToken = jwtProvider.generateToken(username);
 
-        //3. 권한 검증 ==>AOP 처리?
+            response.setHeader("Authorization", "Bearer " + newAccessToken);
+            filterChain.doFilter(request, response);
+        } else {
+            unAuthResponse(response);
+        }
     }
 
     private void unAuthResponse(HttpServletResponse response) throws IOException {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 상태 코드 설정
-        response.setContentType("application/json"); // 응답 타입을 JSON으로 설정
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         String jsonResponse = "{\"error\": \"토큰이 만료되었습니다.\"}";
-        response.getWriter().write(jsonResponse); // 응답 본문에 JSON 메시지 작성
+        response.getWriter().write(jsonResponse);
     }
 }
