1bde1900d696a8d682712d506841eb5a130fcd3c
GoUiJeong <uijeong0525@gmail.com>
2024-11-05 16:48:10 +0900

    🩹 fix : 리스트로 받게 수정했습니다.

--- src/main/java/com/sparta/realtomatoapp/store/controller/StoreController.java
+++ src/main/java/com/sparta/realtomatoapp/store/controller/StoreController.java
@@ -1,6 +1,7 @@
 package com.sparta.realtomatoapp.store.controller;
 
-import com.sparta.realtomatoapp.common.dto.ApiResponseDto;
+import com.sparta.realtomatoapp.common.dto.BaseResponseDto;
+import com.sparta.realtomatoapp.common.dto.DataResponseDto;
 import com.sparta.realtomatoapp.store.dto.*;
 import com.sparta.realtomatoapp.store.service.StoreService;
 import lombok.RequiredArgsConstructor;
@@ -17,32 +18,32 @@ public class StoreController {
     private final StoreService storeService;
 
     @PostMapping
-    public ResponseEntity<ApiResponseDto<StoreCreateResponseDto>> createStore(@RequestBody StoreCreateRequestDto requestDto) {
+    public ResponseEntity<DataResponseDto<StoreCreateResponseDto>> createStore(@RequestBody StoreCreateRequestDto requestDto) {
         StoreCreateResponseDto storeData = storeService.createStore(requestDto);
-        return ResponseEntity.ok(new ApiResponseDto<>("가게 생성 성공", List.of(storeData)));
+        return ResponseEntity.ok(new DataResponseDto<>("가게 생성 성공", List.of(storeData)));
     }
 
     @GetMapping("/{storeId}")
-    public ResponseEntity<ApiResponseDto<StoreResponseDto>> getStore(@PathVariable Long storeId) {
+    public ResponseEntity<DataResponseDto<StoreResponseDto>> getStore(@PathVariable Long storeId) {
         StoreResponseDto storeData = storeService.getStoreById(storeId);
-        return ResponseEntity.ok(new ApiResponseDto<>("가게 단건 조회 성공", List.of(storeData)));
+        return ResponseEntity.ok(new DataResponseDto<>("가게 단건 조회 성공", List.of(storeData)));
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponseDto<StoreListResponseDto>> getAllStores() {
+    public ResponseEntity<DataResponseDto<StoreListResponseDto>> getAllStores() {
         List<StoreListResponseDto> storeDataList = storeService.getAllStores();
-        return ResponseEntity.ok(new ApiResponseDto<>("가게 다건 조회 성공", storeDataList));
+        return ResponseEntity.ok(new DataResponseDto<>("가게 다건 조회 성공", storeDataList));
     }
 
     @PutMapping("/{storeId}")
-    public ResponseEntity<ApiResponseDto<StoreUpdateResponseDto>> updateStore(@PathVariable Long storeId, @RequestBody StoreUpdateRequestDto requestDto) {
+    public ResponseEntity<DataResponseDto<StoreUpdateResponseDto>> updateStore(@PathVariable Long storeId, @RequestBody StoreUpdateRequestDto requestDto) {
         StoreUpdateResponseDto storeData = storeService.updateStore(storeId, requestDto);
-        return ResponseEntity.ok(new ApiResponseDto<>("가게 정보 수정 성공", List.of(storeData)));
+        return ResponseEntity.ok(new DataResponseDto<>("가게 정보 수정 성공", List.of(storeData)));
     }
 
     @DeleteMapping("/{storeId}")
-    public ResponseEntity<ApiResponseDto<StoreDeleteResponseDto>> deleteStore(@PathVariable Long storeId) {
+    public ResponseEntity<BaseResponseDto> deleteStore(@PathVariable Long storeId) {
         StoreDeleteResponseDto storeData = storeService.deleteStore(storeId);
-        return ResponseEntity.ok(new ApiResponseDto<>("가게 삭제 완료", List.of(storeData)));
+        return ResponseEntity.ok(BaseResponseDto.baseResponseBuilder().message(storeData.getMessage()).build());
     }
 }
