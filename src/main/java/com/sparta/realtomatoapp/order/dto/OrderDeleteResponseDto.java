adee35937c93edbe6f030b6e786a88e6ee376b30
GoUiJeong <uijeong0525@gmail.com>
2024-11-05 19:39:13 +0900

    🎉 add : OrderDeleteResponseDto 추가

---
+++ src/main/java/com/sparta/realtomatoapp/order/dto/OrderDeleteResponseDto.java
@@ -0,0 +1,12 @@
+package com.sparta.realtomatoapp.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDeleteResponseDto {
+    private String message;
+}
