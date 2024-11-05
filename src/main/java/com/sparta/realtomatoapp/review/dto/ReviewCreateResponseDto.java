42c15cd494c8ec5a7cfa96f086c07f77564c3c7d
gebeen-dev <rrdd1207@gmail.com>
2024-11-05 20:20:57 +0900

    add :  ReviewCreateResponseDto 생성

--- src/main/java/com/sparta/realtomatoapp/review/dto/ReviewCreateResponseDto.java
+++ src/main/java/com/sparta/realtomatoapp/review/dto/ReviewCreateResponseDto.java
@@ -1,4 +1,10 @@
 package com.sparta.realtomatoapp.review.dto;
 
 public class ReviewCreateResponseDto {
+
+private Long reviewId;
+private Long orderId;
+private String comment;
+private int rating;
+
 }
