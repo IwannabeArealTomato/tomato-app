d550efa906edb9686e88e37ae22b0ad517191bd2
GoUiJeong <uijeong0525@gmail.com>
2024-11-05 16:56:56 +0900

    fix : 충돌해결

--- src/main/java/com/sparta/realtomatoapp/common/dto/ApiResponseDto.java
+++ src/main/java/com/sparta/realtomatoapp/common/dto/ApiResponseDto.java
@@ -1,18 +1,13 @@
 package com.sparta.realtomatoapp.common.dto;
 
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 
 import java.util.List;
 
-@Data
+@Getter
 @Builder
-public class ApiResponseDto<T> {
+public class ApiResponseDTO<T> {
     private String message;
     private List<T> data;
-
-    public ApiResponseDto(String message, List<T> data) {
-        this.message = message;
-        this.data = data;
-    }
 }
