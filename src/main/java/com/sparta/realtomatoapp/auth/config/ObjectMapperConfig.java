e3f871d0f055ada800d65bf780a48e17c8adbd61
hwk9407 <hwk__@naver.com>
2024-11-05 19:58:21 +0900

    🐛 bugfix: ObjectMapper가 LocalDateTime 객체를 직렬화 하지 못하는 버그 수정

--- src/main/java/com/sparta/realtomatoapp/auth/config/ObjectMapperConfig.java
+++ src/main/java/com/sparta/realtomatoapp/auth/config/ObjectMapperConfig.java
@@ -2,6 +2,7 @@ package com.sparta.realtomatoapp.auth.config;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,8 @@ public class ObjectMapperConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+        // 타임스탬프 대신 ISO-8601 포맷으로 날짜 직렬화
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
     }
 }
