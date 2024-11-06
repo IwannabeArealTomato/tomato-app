ef49c9ff5dfc0385c9e7329130aeaa2057fde22d
hwk9407 <hwk__@naver.com>
2024-11-06 18:58:50 +0900

    🩹 fix: 불필요한 주석 및 import 제거

--- src/main/java/com/sparta/realtomatoapp/auth/controller/AuthController.java
+++ src/main/java/com/sparta/realtomatoapp/auth/controller/AuthController.java
@@ -1,23 +1,23 @@
 package com.sparta.realtomatoapp.auth.controller;
 
+import com.sparta.realtomatoapp.auth.dto.LoginRequestDto;
 import com.sparta.realtomatoapp.auth.dto.LoginTokenResponseDto;
 import com.sparta.realtomatoapp.auth.dto.UserRegistrationRequestDto;
 import com.sparta.realtomatoapp.auth.dto.UserResponseDto;
 import com.sparta.realtomatoapp.common.dto.BaseResponseDto;
 import com.sparta.realtomatoapp.common.dto.DataResponseDto;
-import com.sparta.realtomatoapp.auth.dto.LoginRequestDto;
 import com.sparta.realtomatoapp.security.config.JwtProvider;
 import com.sparta.realtomatoapp.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -67,38 +67,4 @@ public class AuthController {
                 .data(Collections.singletonList(userResponseDto))
                 .build());
     }
-
-    /*@PostMapping("/refresh")
-    public ResponseEntity<BaseResponseDto> refreshAccessToken(@RequestHeader("Refresh-Token") String refreshToken) {
-        log.info("AuthController.refreshAccessToken");
-
-        if (!userService.verifyRefreshToken(refreshToken)) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN) // 변경
-                    .body(BaseResponseDto.baseResponseBuilder()
-                            .message("유효하지 않은 또는 만료된 Refresh Token입니다.")
-                            .build());
-        }
-
-        String newAccessToken = userService.refreshAccessToken(refreshToken);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + newAccessToken);
-
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(BaseResponseDto.baseResponseBuilder()
-                        .message("Access Token 재발급 성공")
-                        .build());
-    }*/
-
-    /*@PostMapping("/logout")
-    public ResponseEntity<BaseResponseDto> logout(@RequestHeader("Refresh-Token") String refreshToken) {
-        log.info("AuthController.logout");
-
-        userService.invalidateRefreshToken(refreshToken);
-
-        return ResponseEntity.ok(BaseResponseDto.baseResponseBuilder()
-                .message("로그아웃 성공")
-                .build());
-    }*/
 }
